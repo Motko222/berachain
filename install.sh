93f7dc0a74e7e248e335e4e2e565af09868cdc90
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-24 15:31:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r dependencies
 deactivate
+
+read -p "Capsolver key? $key
+
+echo "CAPSOLVER_KEY="$key > ~/scripts/bera-faucet/config/env
